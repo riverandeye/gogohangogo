634fac2416884beda5185524904afba3ccb321ca
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-01 20:44:17 +0900

    add constants for mail service

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -52,6 +52,7 @@ export const DB_COLUMN = {
 export const API_PARAM = {
   USER: {
     ID: `/:${ID}`,
+    VERIFY: '/verify'
   },
   PARTIES: {
     ID: `/:${ID}`,
@@ -65,6 +66,7 @@ export const API_ROUTE = {
 
 export const REGEX = {
   NATURAL_NUMBER: /^[1-9]\d*$/,
+  EMAIL: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
 
 export const ERROR_CODE = {
@@ -73,10 +75,22 @@ export const ERROR_CODE = {
 
 export const ERROR_MESSAGE = {
   INVALID_ID: `올바르지 않은 ID로 요청하셨습니다.`,
+  INVALID_EMAIL: `올바르지 않은 이메일입니다.`,
+  INVALID_TYPE: `올바르지 않은 타입입니다.`
 };
 
 export const ERROR_RESPONSE = {
   INVALID_ID: {
     message: ERROR_MESSAGE.INVALID_ID,
   },
+  INVALID_EMAIL : {
+    message: ERROR_MESSAGE.INVALID_EMAIL,
+  }, 
+  INVALID_TYPE: {
+    message: ERROR_MESSAGE.INVALID_TYPE,
+  }
 };
+
+export const MAIL = {
+  MAIL_SUBJECT: `엔브레드 서비스 이메일 인증 메시지 입니다.`
+}
