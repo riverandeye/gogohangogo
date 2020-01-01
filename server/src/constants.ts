b9842ac95c7fa7993690d84d7dc3eddfe9bff87d
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-02 00:33:07 +0900

    add constant

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,6 @@
 export const ID = 'id';
 export const NAME = 'name';
+export const AUTHKEY = 'authKey';
 
 export const DB_TABLE = {
   USERS: 'Users',
@@ -14,6 +15,8 @@ export const DB_COLUMN = {
     ID,
     NAME,
     EMAIL: 'email',
+    AUTHKEY: 'authKey',
+    PASSWORD: 'password',
     AVATAR: 'avatar',
     AGREEALARM: 'agreeAlarm',
     PRIORITY: 'priority',
@@ -52,8 +55,10 @@ export const DB_COLUMN = {
 export const API_PARAM = {
   USER: {
     ID: `/:${ID}`,
-    VERIFY: '/verify'
+    VERIFY: `/verify/:${AUTHKEY}`,
+    REVERIFY: `/reverify/:${ID}`,
   },
+
   PARTIES: {
     ID: `/:${ID}`,
   },
@@ -69,28 +74,44 @@ export const REGEX = {
   EMAIL: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
 
-export const ERROR_CODE = {
+export const STATUS_CODE = {
+  OK: 200,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
   UNPROCESSIBLE_ENTITY: 422,
+  SERVER_ERROR: 500,
 };
 
-export const ERROR_MESSAGE = {
+const ERROR_MESSAGE = {
   INVALID_ID: `올바르지 않은 ID로 요청하셨습니다.`,
   INVALID_EMAIL: `올바르지 않은 이메일입니다.`,
-  INVALID_TYPE: `올바르지 않은 타입입니다.`
+  INVALID_TYPE: `올바르지 않은 타입입니다.`,
+  DUPLICATE_EMAIL: `이미 존재하는 이메일입니다.`,
+  INVALID_AUTH_KEY: `유효하지 않은 인증 키입니다.`,
+  NO_REQUIRED_FILED: `필요 필드가 존재하지 않습니다.`,
 };
 
 export const ERROR_RESPONSE = {
   INVALID_ID: {
     message: ERROR_MESSAGE.INVALID_ID,
   },
-  INVALID_EMAIL : {
+  INVALID_EMAIL: {
     message: ERROR_MESSAGE.INVALID_EMAIL,
-  }, 
+  },
   INVALID_TYPE: {
     message: ERROR_MESSAGE.INVALID_TYPE,
-  }
+  },
+  DUPLICATE_EMAIL: {
+    message: ERROR_MESSAGE.DUPLICATE_EMAIL,
+  },
+  INVALID_AUTH_KEY: {
+    message: ERROR_MESSAGE.INVALID_AUTH_KEY,
+  },
+  NO_REQUIRED_FILED: {
+    message: ERROR_MESSAGE.NO_REQUIRED_FILED,
+  },
 };
 
 export const MAIL = {
-  MAIL_SUBJECT: `엔브레드 서비스 이메일 인증 메시지 입니다.`
-}
+  MAIL_SUBJECT: `[NBread] 이메일 인증`,
+};
