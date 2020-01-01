4a42c01ba67ba6b15620d711a5389152b3809533
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-02 00:35:39 +0900

    implement email service

--- server/src/service/mail.ts
+++ server/src/service/mail.ts
@@ -1,37 +1,40 @@
 import nodemailer from 'nodemailer';
 import smtpPool from 'nodemailer-smtp-transport';
 import { MAIL } from '../constants';
+import generateMailContent from '../utils/generate-mail-content';
+
+console.log(process.env.MAIL_ID);
 
 const stmpConfig = {
-    mailer: {
-        service : "Gmail",
-        host: 'localhost',
-        port: '465',
-        auth: {
-            user: process.env.MAIL_ID,
-            password: process.env.MAIL_PW
-        }
-    }
-}
+  mailer: {
+    service: 'Gmail',
+    host: 'localhost',
+    port: '465',
+    auth: {
+      user: process.env.MAIL_ID,
+      pass: process.env.MAIL_PW,
+    },
+  },
+};
 
-const transporter = nodemailer.createTransport(smtpPool(stmpConfig.mailer))
+const transporter = nodemailer.createTransport(smtpPool(stmpConfig.mailer));
 
 const EmailService = {
-    async sendVerificationMail(email: string) {
-        const mailOptions = {
-            from: process.env.MAIL_ID,
-            to: email,
-            subject: MAIL.MAIL_SUBJECT,
-            text: 'this is test email.'
-        }
-        
-        const info = await transporter.sendMail(mailOptions);
+  async sendVerificationMail(email: string, authKey: string) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: MAIL.MAIL_SUBJECT,
+      html: generateMailContent(authKey),
+    };
 
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        
-        return info;
-    } 
-}
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    return info;
+  },
+};
 
 export default EmailService;
