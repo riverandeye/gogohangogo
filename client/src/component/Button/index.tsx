d4595f7b9685f9358d4562f41edcd282790017dd
hanameee <hanameee@likelion.org>
2020-01-03 15:31:55 +0900

    Add Button Component

---
+++ client/src/component/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './styles';
+const Button: React.FC = ({ children }) => {
+  return <S.Button>{children}</S.Button>;
+};
+
+export default Button;
---
+++ client/src/component/Button/styles.tsx
@@ -0,0 +1,11 @@
+import MaterialButton from '@material-ui/core/Button';
+import styled from 'styled-components/macro';
+
+export const Button = styled(MaterialButton)`
+  && {
+    font-family: 'NanumSquare', sans-serif;
+    background-color: #f94e36;
+    font-weight: 700;
+    color: #ffffff;
+  }
+`;
