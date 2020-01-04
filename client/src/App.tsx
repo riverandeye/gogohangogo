388a38d2521f4bad75adf735b8a95bfcd5cd6a4a
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-05 04:27:08 +0900

    f: redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Landing from './page/landing';
 import { ROUTE } from './constants';
+import PrivateRoute from './component/PrivateRoute';
 import Main from './page/main';
 import Subscribe from './page/subscribe';
 import SignUp from './page/signup';
@@ -16,7 +17,12 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path={ROUTE.LANDING} component={Landing} />
-        <Route exact path={ROUTE.MAIN} component={Main} />
+        <PrivateRoute
+          exact
+          path={ROUTE.MAIN}
+          component={Main}
+          redirectTo={ROUTE.LANDING}
+        />
         <Route exact path={ROUTE.SUBSCRIBE} component={Subscribe} />
         <Route exact path={ROUTE.SIGNUP} component={SignUp} />
         <Route exact path={ROUTE.MYPARTIES} component={MyParties} />
