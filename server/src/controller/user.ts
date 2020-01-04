4db73b56d3c18a89714aa9829fcf40dc309319ff
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-04 12:54:58 +0900

    b: move business logic from controller to service

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -16,6 +16,7 @@ import { User } from '../model/Interface/user';
 
 import UserService from '../service/user';
 import pushAlarmService from '../service/push-alarm';
+import { ERRORS } from '../errors';
 
 webpush.setGCMAPIKey(process.env.GOOGLE_API_KEY);
 webpush.setVapidDetails(
@@ -36,27 +37,28 @@ const UserController = {
     const Id = Number(req.params[ID]);
     const subscription = req.body;
 
-    const User = await UserService.updateUserSubscription(Id, subscription);
+    const user = await UserService.updateUserSubscription(Id, subscription);
     const result = await pushAlarmService.sendPushAlarmOnce(
       subscription,
       ALARM_MESSAGE.SUBSCRIBE_SUCCESS,
     );
 
-    res.json(User);
+    res.json(user);
   },
 
   async unSubscribeAlarm(req: Request, res: Response, next: NextFunction) {
     const Id = Number(req.params[ID]);
-    const User = await UserService.deleteUserSubscription(Id);
+    const user = await UserService.deleteUserSubscription(Id);
 
-    res.json(User);
+    res.json(user);
   },
 
   async checkAlarm(req: Request, res: Response, next: NextFunction) {
     const Id = Number(req.params[ID]);
-    const User = await UserService.findUserWithId(Id);
+    const user = await UserService.findUserWithId(Id);
+
     const result = await pushAlarmService.sendPushAlarmOnce(
-      JSON.parse(User.alarmSubscription),
+      JSON.parse(user.alarmSubscription),
       ALARM_MESSAGE.CHECK_SUBSCRIBE,
     );
     res.status(200).send();
@@ -66,7 +68,9 @@ const UserController = {
     const Id = Number(req.params[ID]);
     const updatedUser = req.body;
     await UserModel.updateUser(Id, updatedUser);
-    res.send(await UserModel.getUserWithId(Id));
+    const user = await UserService.findUserWithId(Id);
+
+    res.send(user);
   },
 
   // create user
@@ -92,34 +96,34 @@ const UserController = {
     createdUser.authKey = uuid();
     createdUser.alarmSubscription = {};
 
-    await UserModel.createUser(createdUser);
+    await UserService.createUser(createdUser);
 
     // send verification email
     await EmailService.sendVerificationMail(
       createdUser.email,
       createdUser.authKey,
     );
-    res.send(await UserModel.getUserWithEmail(createdUser.email));
+
+    res.status(STATUS_CODE.CREATED);
   },
 
   // verify
   async verifyUser(req: Request, res: Response, next: NextFunction) {
     const authKey = String(req.params[AUTHKEY]);
-    const users: User[] = await UserModel.getUserWithAuthKey(authKey);
-    if (users.length !== 1) {
-      res.status(STATUS_CODE.BAD_REQUEST).send(ERROR_RESPONSE.INVALID_AUTH_KEY);
-      return;
-    }
-    await UserModel.updateUser(users[0].id, { isValid: true });
+    try {
+      const user: User = await UserService.verifyUser(authKey);
 
-    // TODO: need to be add our site url
-    res
-      .status(STATUS_CODE.OK)
-      .redirect(
-        `http://${process.env.SERVER_HOST}:${
-          process.env.SERVER_PORT
-        }/users/${String(users[0].id)}/`,
-      );
+      // TODO: need to be add our site url
+      res
+        .status(STATUS_CODE.OK)
+        .redirect(
+          `http://${process.env.SERVER_HOST}:${
+            process.env.SERVER_PORT
+          }/users/${String(user.id)}/`,
+        );
+    } catch (e) {
+      res.status(STATUS_CODE.BAD_REQUEST).send(ERROR_RESPONSE.INVALID_AUTH_KEY);
+    }
   },
 
   // reverify
@@ -129,7 +133,7 @@ const UserController = {
     next: NextFunction,
   ) {
     const Id = Number(req.params[ID]);
-    const user: User = (await UserModel.getUserWithId(Id))[0];
+    const user: User = await UserService.findUserWithId(Id);
 
     res.send(await EmailService.sendVerificationMail(user.email, user.authKey));
   },
