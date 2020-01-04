1da183f8c9dd97bcb9a9f266c948c0d12948a142
minju <52149296+minzoovv@users.noreply.github.com>
2020-01-04 16:55:55 +0900

    f: add constant for validation error

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -10,3 +10,16 @@ export const ROUTE = {
 export const SERVER_ENDPOINT = {
   SUBSCRIBE: `${process.env.REACT_APP_BACKEND_HOST}/users/subscribe`,
 };
+
+export const VALIDATION_ERROR_MESSAGE = {
+  INVALID_EMAIL: '유효한 이메일이 아닙니다.',
+  REQUIRED_EMAIL: '이메일을 입력해주세요.',
+  REQUIRED_NAME: '이름을 입력해주세요.',
+  REQUIRED_PW: '비밀번호를 입력해주세요.',
+  MIN_8: '8글자 이상으로 입력해주세요.',
+  NON_UNIV_EMAIL: '학교 계정이 아닙니다.',
+};
+
+export const REGEX = {
+  UNIV_EMAIL: /ac.kr$/,
+};
