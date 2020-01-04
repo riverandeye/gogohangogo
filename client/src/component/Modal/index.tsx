889f72142d6009c599894b4681dec7d68f871618
JeongHo Nam <rainy_waltz@naver.com>
2020-01-05 06:13:52 +0900

    f: 파티 참가 핸들러 추가

--- client/src/component/Modal/index.tsx
+++ client/src/component/Modal/index.tsx
@@ -5,6 +5,7 @@ import SendRoundedIcon from '@material-ui/icons/SendRounded';
 import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import PersonOutlineRoundedIcon from '@material-ui/icons/PersonOutlineRounded';
+import Axios from 'axios';
 interface ModalProps {
   isOpened?: Boolean;
   modalInfo;
@@ -38,81 +39,102 @@ const Modal: React.FC<ModalProps> = ({
   for (let i = 0; i < personnel; i++) {
     takenArr.push({});
   }
-  if (isOpened) {
-    return (
-      <S.main>
-        <S.Background onClick={closeModalHandler}></S.Background>
-        <MDBAnimation
-          type="bounceInUp"
-          style={{
-            position: 'fixed',
-            zIndex: '200',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-          }}
-        >
-          <S.PopupBackground>
-            <S.ModalContent>
-              <S.ModalTitle>
-                <span style={{ color: '#f94e36' }}>{ottName}</span> 서비스를
-                공유할
-                <br />
-                {title} 팟에 가입하시겠어요?
-              </S.ModalTitle>
-              <S.ModalIntroduction></S.ModalIntroduction>
-              <ul>
-                <li>이 팟을 만든 팟장 id는 {adminUserId}</li>
-                <li>이 팟에 가입 신청한 사람들은 {personnel}명</li>
-                {slotArr.map(ico => (
-                  <PersonOutlineRoundedIcon
-                    style={{ color: '#f94e36', fontSize: '4rem' }}
-                  >
-                    {ico}
-                  </PersonOutlineRoundedIcon>
-                ))}
-                {takenArr.map(ico => (
-                  <PersonRoundedIcon
-                    style={{ color: '#808080', fontSize: '4rem' }}
-                  >
-                    {ico}
-                  </PersonRoundedIcon>
-                ))}
-                <S.ModalMessage>
-                  아직 {maxSlot}자리 중{' '}
-                  <span style={{ color: '#f94e36', fontWeight: 700 }}>
-                    {availableSlot}
-                  </span>
-                  자리 남았어요!
-                </S.ModalMessage>
-              </ul>
-            </S.ModalContent>
-            <S.ButtonContainer>
-              <S.Button
-                variant="contained"
-                className="success"
-                style={{ border: '0', outline: '0' }}
-                endIcon={<SendRoundedIcon />}
-              >
-                이 팟 가입 요청하기
-              </S.Button>
-              <S.Button
-                variant="contained"
-                className="fail"
-                onClick={closeModalHandler}
-                style={{ border: '0', outline: '0' }}
-                endIcon={<CancelRoundedIcon />}
-              >
-                다른 팟들 더 둘러보기
-              </S.Button>
-            </S.ButtonContainer>
-          </S.PopupBackground>
-        </MDBAnimation>
-      </S.main>
+
+  const dummyMyUserId = 1;
+
+  const participatePartyHandler = () => {
+    const response1 = Axios.patch(
+      `${process.env.REACT_APP_BACKEND_HOST}/parties/${partyCardId}`,
+      {
+        personnel: personnel + 1,
+      },
     );
-  } else {
-    return <div></div>;
-  }
+
+    const response2 = Axios.post(
+      `${process.env.REACT_APP_BACKEND_HOST}/users/${dummyMyUserId}/parties`,
+      {
+        userId: dummyMyUserId,
+        partyId: partyCardId,
+      },
+    );
+    Promise.all([response1, response2]).then(values => {
+      console.log('팟 참가 완료');
+    });
+  };
+
+  if (!isOpened) return null;
+
+  return (
+    <S.main>
+      <S.Background onClick={closeModalHandler}></S.Background>
+      <MDBAnimation
+        type="bounceInUp"
+        style={{
+          position: 'fixed',
+          zIndex: '200',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <S.PopupBackground>
+          <S.ModalContent>
+            <S.ModalTitle>
+              <span style={{ color: '#f94e36' }}>{ottName}</span> 서비스를
+              공유할
+              <br />
+              {title} 팟에 가입하시겠어요?
+            </S.ModalTitle>
+            <S.ModalIntroduction></S.ModalIntroduction>
+            <ul>
+              <li>이 팟을 만든 팟장 id는 {adminUserId}</li>
+              <li>이 팟에 가입 신청한 사람들은 {personnel}명</li>
+              {slotArr.map(ico => (
+                <PersonOutlineRoundedIcon
+                  style={{ color: '#f94e36', fontSize: '4rem' }}
+                >
+                  {ico}
+                </PersonOutlineRoundedIcon>
+              ))}
+              {takenArr.map(ico => (
+                <PersonRoundedIcon
+                  style={{ color: '#808080', fontSize: '4rem' }}
+                >
+                  {ico}
+                </PersonRoundedIcon>
+              ))}
+              <S.ModalMessage>
+                아직 {maxSlot}자리 중{' '}
+                <span style={{ color: '#f94e36', fontWeight: 700 }}>
+                  {availableSlot}
+                </span>
+                자리 남았어요!
+              </S.ModalMessage>
+            </ul>
+          </S.ModalContent>
+          <S.ButtonContainer>
+            <S.Button
+              variant="contained"
+              className="success"
+              style={{ border: '0', outline: '0' }}
+              endIcon={<SendRoundedIcon />}
+            >
+              이 팟 가입 요청하기
+            </S.Button>
+            <S.Button
+              variant="contained"
+              className="fail"
+              onClick={closeModalHandler}
+              style={{ border: '0', outline: '0' }}
+              endIcon={<CancelRoundedIcon />}
+            >
+              다른 팟들 더 둘러보기
+            </S.Button>
+          </S.ButtonContainer>
+        </S.PopupBackground>
+      </MDBAnimation>
+    </S.main>
+  );
 };
 
 export default Modal;
